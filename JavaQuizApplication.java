78b391fc221372ddab46af4cc442aea6041d37f6
Ch Laasya Priya <151950570+chlaasyapriya@users.noreply.github.com>
2024-08-19 22:29:32 +0530

    Add files via upload

---
+++ JavaQuizApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaquizapplication;
+
+/**
+ *
+ * @author MOUKTHI
+ */
+public class JavaQuizApplication {
+    // this is where the application starts running
+    public static void main(String[] args) {
+        new Start().setVisible(true);
+    }
+}
